0ad5e8d2a16d26ea49c8fc0cd1dc9fd67efc5142
Benny Wong <bdotdub@gmail.com>
2009-11-28 00:05:32 -0500

    Whoa big commit. Also a lot of it is converting CRLF
    * Added function to traverse each tab and stick it in a hash
    * AJAX function
    * Serializer for the data
    * Adding stubs for logging
    * Removing stuff I don't "need" right now to simplify development
    
    PS: This is what happens when you're back home for Thanksgiving

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -5,18 +5,81 @@ var bwongsink = {
     this.strings = document.getElementById("bwongsink-strings");
   },
 
-  onMenuItemCommand: function(e) {
-    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-                                  .getService(Components.interfaces.nsIPromptService);
-    promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                this.strings.getString("helloMessage"));
-    alert('hi');
-  },
+  sinkTabs: function() {
+    var windows = [];
 
-  onToolbarButtonCommand: function(e) {
-    // just reuse the function above.  you can change this, obviously!
-    bwongsink.onMenuItemCommand(e);
+    var windowMediator = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
+    var windowEnumerator = windowMediator.getEnumerator('navigator:browser');
+
+    while (windowEnumerator.hasMoreElements()) {
+      var tabs = []
+
+      var browserWindow = windowEnumerator.getNext();
+      var tabBrowser    = browserWindow.getBrowser();
+
+      for (var idx = 0; idx < tabBrowser.browsers.length; ++idx) {
+        var tab = tabBrowser.getBrowserAtIndex(idx);
+        tabs.push({
+          'uri'       : tab.currentURI.spec,
+          'selected'  : (tabBrowser.selectedTab == tabBrowser.mTabs[idx])
+        });
+      }
+      windows.push(tabs);
+    }
+
+    bwongsink.server.sync(windows);
   }
 };
 
+bwongsink.logging = {
+  debug: function(msg) {
+
+  },
+
+  error: function(msg) {
+
+  },
+
+  info: function(msg) {
+
+  }
+}
+
+bwongsink.server = {
+  fullUrl: "http://localhost/bwong/test.php",
+  httpRequest: null,
+
+  requestLoaded: function() {
+    bwongsink.logging.info(bwongsink.server.httpRequest.responseText);
+  },
+
+  serialize: function(windows) {
+    var paramsData = [];
+    for (var winIdx = 0; winIdx < windows.length; ++winIdx) {
+      var tabs = windows[winIdx];
+      for (var tabIdx = 0; tabIdx < tabs.length; ++tabIdx) {
+        var uriKey = 'window_' + winIdx + '_tab_' + tabIdx + '_uri';
+        paramsData.push(uriKey + "=" + escape(tabs[tabIdx]['uri']));
+
+        var selectedKey = 'window_' + winIdx + '_tab_' + tabIdx + '_selected';
+        paramsData.push(selectedKey + "=" + escape(tabs[tabIdx]['selected']));
+      }
+    }
+
+    return paramsData.join('&')
+  },
+
+  sync: function(windows) {
+    var serializedParams = bwongsink.server.serialize(windows);
+    bwongsink.server.httpRequest = new XMLHttpRequest();
+
+    bwongsink.server.httpRequest.open("POST", bwongsink.server.fullUrl, true);
+    bwongsink.server.httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+
+    bwongsink.server.httpRequest.onload = bwongsink.server.requestLoaded;
+    bwongsink.server.httpRequest.send(serializedParams);
+  }
+}
+
 window.addEventListener("load", bwongsink.onLoad, false);
+
